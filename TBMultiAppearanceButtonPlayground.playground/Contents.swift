07f39f8941698434824f23f681030f214e656c5d
Clay Smith <s.clay.smith@gmail.com>
2015-07-01 23:59:59 -0700

    Updated playground to match current methods.

--- TBMultiAppearanceButtonPlayground.playground/Contents.swift
+++ TBMultiAppearanceButtonPlayground.playground/Contents.swift
@@ -27,17 +27,28 @@ struct PlayerControlAppearance: TBControlAppearanceType {
 let button = TBMultiAppearanceButton<PlayerControlAppearance>(frame: CGRectMake(0, 0, 100, 100))
 
 button.setTitle("Play", forAppearance: .Play, andState: .Normal)
-button.setTitle("Clicked play", forAppearance: .Play, andState: .Highlighted)
+button.setTitle("Play", forAppearance: .Play, andState: .Highlighted)
 
 button.setBackgroundImage(UIImage.imageWithColor(UIColor.blueColor()), forAppearance: .Play, andState: .Normal)
 button.setBackgroundImage(UIImage.imageWithColor(UIColor.lightGrayColor()), forAppearance: .Play, andState: .Highlighted)
 
 button.setTitle("Pause", forAppearance: .Pause, andState: .Normal)
+button.setTitleColor(UIColor.blueColor(), forAppearance: .Pause, andState: .Normal)
 button.setBackgroundImage(UIImage.imageWithColor(UIColor.yellowColor()), forAppearance: .Pause, andState: .Normal)
 
 button.setTitle("Stop", forAppearance: .Stop, andState: .Normal)
 
 button.activateAppearance(.Play)
 
+button
+
+button.highlighted = true
 
 button
+
+button.highlighted = false
+button.activateAppearance(.Pause)
+
+button
+
+
